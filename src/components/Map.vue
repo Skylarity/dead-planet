63d927c0f48818f185cbedd4a29a3958aed66d5c
Skyler Rexroad <skyrex1095@gmail.com>
2017-07-08 12:25:20 -0600

    Added map component

---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'map',
+  data () {
+    return {
+      // TODO
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+	#map {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
